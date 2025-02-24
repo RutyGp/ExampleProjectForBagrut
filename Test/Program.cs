32b2a0852f49245bfaaac9527bd7548d62169a79
רותי גולקין <nirgolkin@gmail.com>
2025-02-25 00:04:39 +0200

    Add PersonDB

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using ViewModel;
+
+CityDB cdb = new();
+CityList cList = cdb.SelectAll();
+foreach (City c in cList)
+    Console.WriteLine(c.CityName);
+
+#region
+
+PersonDB pdb = new();
+PersonList pList = pdb.SelectAll();
+foreach (Person c in pList)
+    Console.WriteLine(c.FirstName);
+#endregion
